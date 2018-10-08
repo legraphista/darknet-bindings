a34b03dca65e1caf299666e96432e6af47bc4bf8
Ștefan-Gabriel Muscalu <legraphista@users.noreply.github.com>
2018-10-08 17:03:48 +0300

    Fix: lib so linker

--- libs.js
+++ libs.js
@@ -3,9 +3,5 @@ const path = require('path');
 const rootDir = __dirname;
 
 console.log([
-  path.join(rootDir, 'libdarknet.so')
-].join( ' '));
-
-// console.log(`\
-//   ${path.join(rootDir, 'libdarknet.so')} \\
-// `);
+  `-Wl,-rpath,${path.join(rootDir, 'libdarknet.so')}`
+].join(' '));
