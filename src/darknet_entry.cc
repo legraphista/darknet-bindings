fa4ce3223e5b140fbf03560163d71d2b55b6c09e
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2018-11-09 15:13:59 +0200

    Add: warning in entry abount node 10 instability

--- src/darknet_entry.cc
+++ src/darknet_entry.cc
@@ -2,11 +2,18 @@
 // Created by Muscalu Stefan-Gabriel on 2018-11-08.
 //
 
+#include <node_version.h>
 #include "darknet_entry.h"
 
 using namespace Napi;
 
 Object Init(Napi::Env env, Object exports) {
+
+  if (NODE_MAJOR_VERSION != 9) {
+    fprintf(stderr, "[Darknet-Bindings] Unsupported major version of node %d detected!\n",
+           NODE_MAJOR_VERSION);
+    fprintf(stderr, "[Darknet-Bindings] Stable tested version is 9.11, you might experience crashes.\n");
+  }
 
   DarknetImage::Init(env, exports);
   DarknetClass::Init(env, exports);
