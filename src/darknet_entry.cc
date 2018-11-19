c6f8427a30e85486c0005f56a0696faca7be4d10
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2018-11-19 19:27:01 +0200

    Update: node instability warning See https://github.com/nodejs/node-addon-api/issues/393#

--- src/darknet_entry.cc
+++ src/darknet_entry.cc
@@ -9,10 +9,11 @@ using namespace Napi;
 
 Object Init(Napi::Env env, Object exports) {
 
-  if (NODE_MAJOR_VERSION != 9) {
+  if (NODE_MAJOR_VERSION == 10 || NODE_MAJOR_VERSION == 11) {
     fprintf(stderr, "[Darknet-Bindings] Unsupported major version of node %d detected!\n",
            NODE_MAJOR_VERSION);
     fprintf(stderr, "[Darknet-Bindings] Stable tested version is 9.11, you might experience crashes.\n");
+    fprintf(stderr, "[Darknet-Bindings] See https://github.com/nodejs/node-addon-api/issues/393 for more details.\n");
   }
 
   DarknetImage::Init(env, exports);
